97f5b58351003fa77bec5dc2d6490d8b538b2043
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2014-12-11 19:47:55 +0500

    Добавил тест размера базы трафика

--- tests/billing/check_traffic_db_size.sh
+++ tests/billing/check_traffic_db_size.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 check() {
+	dbsize="$(du -sh /var/db/buff_traf.gdb | awk '{print $1}')"
+	[ "$dbsize" -lt "$((10*1000*1000))" ]
 }
 
 error() {
+	log "Чрезмерно большая база данных трафика"
 }
 
 . ./lib/drhouse drhouse_main
