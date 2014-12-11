1192595b77528c85bb81e84857bd8edbf780839f
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2014-12-11 18:41:51 +0500

    Проверка radius-traf

--- tests/billing/check_billing_radius_traf_auth_netstat.sh
+++ tests/billing/check_billing_radius_traf_auth_netstat.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 check() {
+	. /cfg/config
+	check_netstat "${app['radius_traf.AUTH_IP']}:${app['radius_traf.AUTH_PORT']}"
 }
 
 error() {
+	log "Недоступен Radius-traf сервер (auth)"
+}
+
+fix() {
+	/etc/init.d/radiusd restart
 }
 
 . ./lib/drhouse drhouse_main
