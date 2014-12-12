fec0ad92880f25d4a179792ff9341a0e11fb9905
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2014-12-12 15:16:35 +0500

    ping'и

--- tests/xge/check_xge_radius_acct_ping.sh
+++ tests/xge/check_xge_radius_acct_ping.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 check() {
+	. /cfg/config
+	ping -qc 1 -w 1 "${radclient['acctserver']%%:*}" &>/dev/null
 }
 
 error() {
+	log "Сервер аккаунтинга ${radclient['acctserver']%%:*} недоступен"
 }
 
-. ./lib/drhouse drhouse_main
+. ./lib/drhouse drhouse_main_nosafemode
