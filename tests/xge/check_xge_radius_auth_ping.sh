6717856cfee237ba2c63b5a54017d110d5ab7e02
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2014-12-12 15:19:29 +0500

    Все тесты для xge портированы

--- tests/xge/check_xge_radius_auth_ping.sh
+++ tests/xge/check_xge_radius_auth_ping.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 check() {
+	. /cfg/config
+	ping -qc 1 -w 1 "${radclient['authserver']%%:*}" &>/dev/null
 }
 
 error() {
+	log "Сервер авторизации ${radclient['authserver']%%:*} недоступен"
 }
 
-. ./lib/drhouse drhouse_main
+. ./lib/drhouse drhouse_main_nosafemode
